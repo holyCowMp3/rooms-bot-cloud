99070188bdf20da39d2aa96623398ff226f59e17
Тарас <taras190789@gmail.com>
2021-10-27 20:27:37 +0300

    cross origin allowed

--- rooms-bot-api/src/main/java/com/example/keycloack/config/SimpleCORSFilter.java
+++ rooms-bot-api/src/main/java/com/example/keycloack/config/SimpleCORSFilter.java
@@ -1,27 +1,16 @@
 package com.example.keycloack.config;
 
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@Component
-public class SimpleCORSFilter implements Filter {
+@Configuration
+public class SimpleCORSFilter implements WebMvcConfigurer {
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-        chain.doFilter(req, res);
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
-
-    @Override
-    public void init(FilterConfig filterConfig) {}
-
-    @Override
-    public void destroy() {}
 }
